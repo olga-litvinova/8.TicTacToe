96ee5eaf3c2ba187060ad83c1068ecb837868cdb
Olyafrontend <litvinova2404ad@gmail.com>
2023-09-18 08:57:34 +0200

    draft#4

--- event.js
+++ event.js
@@ -1,132 +1,164 @@
-const playerSign=function(){
-    this.createPlayer=function(sign){
-        
-    }
-}
 
-// const playerSign = sign;
-// let enemySign; 
-// if (playerSign==="X"){
-//     enemySign = "O";
-// }
-// else{
-//     enemySign = "X";
-// }
+// Game starts with a menu section, where player can choose the sign(x or o) and the enemy(human or robot)
+// following piece of code creates a Toggle Switch functianality(on/off button) both for defining the sign and enemy
 
-
-
-const menu_section=document.querySelector('.menu');
-const game_section=document.querySelector('.game');
-const turn=document.querySelector('.turn');
 const x=document.querySelector('.x');
 const o=document.querySelector('.o');
 const pickSign=document.querySelector('.pickSign');
 const robot=document.querySelector('.robot');
 const human=document.querySelector('.human');
 const pickEnemy=document.querySelector('.pickEnemy');
-const board=document.querySelectorAll('.game-board__field');
-var board_array = [...board];
-const stateElement = document.querySelectorAll('.game-board__state');
-var stateElement_array= [...stateElement]
-// const currentState = stateElement.getAttribute('data-field-state');
-const start=document.querySelector('.buttom');
 let color1="#2A2B2E";
-let color2="#222b30";
 let color3="#87FF65";
-let color4="#e4ff1a";
-let color5="#A4C2A8";
 let isX = true; 
 let isRobot=true;
-let sign="X"
-let enemy="Robot"
-
-x.addEventListener('click', switchBtn);
-o.addEventListener('click', switchBtn);
-robot.addEventListener('click', switchEnemy);
-human.addEventListener('click', switchEnemy);
-start.addEventListener('click', startGame);
-
-function switchBtn(){
-    isX = !isX;
-    if (isX) {
-        x.style.backgroundColor = color3;
-        x.style.color = color1;
-        pickSign.innerHTML="Pick your sign: X";
-        o.style.backgroundColor = color1;
-        o.style.color = color3;
-        sign="X";
-    } else {
-        o.style.backgroundColor = color3;
-        o.style.color = color1;
-        pickSign.innerHTML="Pick your sign: O";
-        x.style.backgroundColor = color1;
-        x.style.color = color3;
-        isX=false;
-        sign="O";
-    }
+let playerSign="X";
+let enemySign="O";
+let playerEnemy="Robot"
+function createToggleFunction(element1,element2, color1, color2, labelText, toggleValue, variableToUpdate ){
+    return function(){  
+        toggleValue = !toggleValue;
+        if(toggleValue){
+            element1.style.backgroundColor = color2;
+            element1.style.color = color1;
+            element2.style.backgroundColor = color1;
+            element2.style.color = color2;
+            labelText.innerHTML = `Pick your ${variableToUpdate}: ${element1.textContent}`;
+            if (variableToUpdate === 'sign') {
+                playerSign = element1.textContent;
+                enemySign = element2.textContent;
+            } else if (variableToUpdate === 'enemy') {
+                playerEnemy = element1.textContent;
+            }
+        }
+        else {
+            element1.style.backgroundColor=color1;
+            element1.style.color=color2;
+            element2.style.backgroundColor=color2;
+            element2.style.color=color1;
+            labelText.innerHTML = `Pick your ${variableToUpdate}: ${element2.textContent}`;
+            if (variableToUpdate === 'sign') {
+                playerSign = element2.textContent;
+                enemySign = element1.textContent;
+            } else if (variableToUpdate === 'enemy') {
+                playerEnemy = element2.textContent;
+            }
+        }
+        return toggleValue;
+    };
 }
 
+const toggleSign = createToggleFunction(x, o, color1, color3, pickSign, isX, 'sign');
+const toggleEnemy = createToggleFunction(robot, human, color1, color3, pickEnemy, isRobot, 'enemy');
+
+x.addEventListener('click', toggleSign);
+o.addEventListener('click', toggleSign);
+robot.addEventListener('click', toggleEnemy);
+human.addEventListener('click', toggleEnemy);
 
 
+// After defining the sign and enemy, we are ready to start our game :)
+// With a click of a start button our board game shows off.
+// Technically one section "menu" displayes as none and next section "game" turns on 
 
-function switchEnemy(){
-    isRobot = !isRobot;
-    if (isRobot) {
-        robot.style.backgroundColor = color3;
-        robot.style.color = color1;
-        pickEnemy.innerHTML="Pick your enemy: Robot";
-        human.style.backgroundColor = color1;
-        human.style.color = color3;
-        enemy="Robot";
-    } else {
-        robot.style.backgroundColor = color1;
-        robot.style.color = color3;
-        pickEnemy.innerHTML="Pick your enemy: Human";
-        human.style.backgroundColor = color3;
-        human.style.color = color1;
-        isRobot=false;
-        enemy="Human";
-    }
-}
-
+const start=document.querySelector('.buttom');
 function startGame(){
-    console.log(sign,enemy);
+    const menu_section=document.querySelector('.menu');
+    const game_section=document.querySelector('.game');
+    const turn=document.querySelector('.turn');
     menu_section.style.display = "none";
     game_section.style.display ="grid"
     if(playerSign=="O"){turn.innerHTML="Turn: O";}
     else if(playerSign=="X") {turn.innerHTML="Turn: X";}
 }
+start.addEventListener('click', startGame);
 
 
 
-// board.
-// board.addEventListener('mousemove', () =>{
-//     td.style.backgroundColor=black;
-// })
-
-// board_array.forEach((div) => {
-//     div.addEventListener('mouseleave', () =>{
-//             div.style.backgroundColor='red';
-//         })
-// });
-
-
-board_array.forEach((div) => {
-    // div.addEventListener('mousemove', () =>{
-    //     if (stateElement_array == '') {
-    //         div.innerHTML='X';
-    //     }
-    // });
-    // div.addEventListener('mouseleave', () =>{
-    //     if (stateElement_array == '') {
-    //         div.innerHTML='';
-    //     }
-    // });
-    div.addEventListener('click', () =>{
-        if (stateElement_array == '') {
-            div.innerHTML='X';
-            stateElement_array.setAttribute('data-field-state', 'X');
-            field.style.pointerEvents = 'none';
+// function GameController(PlayerX="Player X",PlayerO="Player O"){
+    let players= [
+        {
+            name:"PlayerX",
+            sign: "X",
+            score:0,
+            position:[
+                "", "", "",
+                "","","", 
+                "","", ""]
+        },
+        {
+            name: "PlayerO",
+            sign: "O",
+            score: 0,
+            position:[
+                "", "", "",
+                "","","", 
+                "","", ""]   
         }
+    ];
+
+    let activePlayer=players[0];
+
+    const board=document.querySelectorAll('.board_field');
+    var board_array = [...board];
+    let area = [
+        "", "", "",
+        "","","", 
+        "","", ""
+    ];
+
+    board_array.forEach((div,index) => {
+        div.addEventListener('mousemove', () =>{
+            if (area[index] == "") {
+                div.innerHTML=activePlayer.sign;
+            }
+        });
+        div.addEventListener('mouseleave', () =>{
+            if (area[index] == "") {
+                div.innerHTML='';
+            }
+        });
+        div.addEventListener('click', () =>{
+            if (area[index] == "") {
+                div.innerHTML=activePlayer.sign;
+                getActivePlayer();
+                activePlayer.position[index]=activePlayer.sign;
+                area[index] = activePlayer.sign;
+                switchPlayerTurn();
+            }
+        });
     });
-});
+   
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+    
+    
+    const checkWinner= () => {
+        area.forEach((index) => {
+
+        }
+    )};
+
+// }
+
+// GameController()
+
+
+//Game
+
+
+
+// 012
+// 345
+// 678
+// 036
+// 147
+// 258
+// 048
+// 246
+
+
+
+
